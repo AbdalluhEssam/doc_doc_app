7da8a6cb28977d19da611b78c85ca76bd2530a5a
abdal <abdallhesam100@gmail.com>
2025-01-08 19:20:02 +0200

    added login screen added spacing finished login screen design

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:doc_doc/doc_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'core/routing/app_router.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await ScreenUtil.ensureScreenSize();
+
   runApp(DocApp(appRouter: AppRouter()));
 }
