22c79d479503f18ad8f574bb3c6d774514c46a5d
abdal <abdallhesam100@gmail.com>
2025-01-12 22:21:18 +0200

    add firebase connections

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -7,6 +7,9 @@ import 'core/routing/app_router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
   await ScreenUtil.ensureScreenSize();
   setupGetIt();
   runApp(DocApp(appRouter: AppRouter()));
