60acbc51fad9b76a1137110c86b39f4e0e57c9f6
abdal <abdallhesam100@gmail.com>
2025-01-30 18:26:48 +0200

    add home tor bar and doctor blue

--- lib/features/home/ui/home_screen.dart
+++ lib/features/home/ui/home_screen.dart
@@ -1,3 +1,6 @@
+import 'package:doc_doc/core/helpers/spacing.dart';
+import 'package:doc_doc/features/home/ui/widgets/doctor_blue_container.dart';
+import 'package:doc_doc/features/home/ui/widgets/home_top_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,18 +9,26 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text('Home Screen'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text('This is the Home Screen'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // context.pushNamed(Routes.loginScreen);
-        },
-        child: const Icon(Icons.login),
+      body: SafeArea(
+        child: Container(
+          // width: double.infinity,
+
+          padding: EdgeInsets.fromLTRB(20, 16, 20, 28),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const HomeTopBar(),
+              const DoctorBlueContainer(),
+              verticalSpace(16),
+              // const HomeBottomBar(),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/features/home/ui/widgets/doctor_blue_container.dart
@@ -0,0 +1,66 @@
+import 'package:doc_doc/core/helpers/spacing.dart';
+import 'package:doc_doc/core/theming/colors.dart';
+import 'package:doc_doc/core/theming/styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class DoctorBlueContainer extends StatelessWidget {
+  const DoctorBlueContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 195.h,
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        // fit: StackFit.passthrough,
+        children: [
+          Container(
+            height: 165.h,
+            width: double.maxFinite,
+            padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
+            decoration: BoxDecoration(
+                // color: ColorsManager.mainBlue,
+                borderRadius: BorderRadius.circular(24.r),
+                image: DecorationImage(
+                    image: AssetImage("assets/images/home_blue_pattern.png"))),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Book and\nschedule with\nnearest doctor",
+                  style: TextStyles.font18WhiteMedium,
+                  textAlign: TextAlign.start,
+                ),
+                verticalSpace(8),
+                Expanded(
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(48.r)),
+                        // minimumSize: Size(double.maxFinite, 40.h)
+                      ),
+                      child: Text(
+                        "Find Doctor",
+                        style: TextStyles.font12BlueRegular,
+                      )),
+                )
+              ],
+            ),
+          ),
+          Positioned(
+            top: 0.h,
+            right: 8.w,
+            child: Image.asset(
+              "assets/images/doc.png",
+              height: 200.h,
+              fit: BoxFit.contain,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
